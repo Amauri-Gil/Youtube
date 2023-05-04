94c780632e33c9ccccbc33423fdca9d445723e56
Amauri-Gil <amauri.rodriguez@outlook.com>
2023-05-03 18:07:10 -0600

    Se agregan métodos GET y POST

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
-app = FastAPI()
+myapp = FastAPI()
+
+# Aquí van los métodos GET
 
 
-@app.get('/')
+@myapp.get('/')
 def index():
-    return {'data': {'name': 'Amauri'}}
+    return {'data': 'info about the page'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@myapp.get('/blogs')
+def blog_list(limit: int = 10,
+              published: bool = True,
+              sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@myapp.get('/blog/unpublished')
+def unpublished_blogs_list():
+    return {'data': 'All unpublished blogs'}
+
+
+@myapp.get('/blog/unpublished/{id}')
+def unpublished_blog(id: int):
+    return {'data': id}
+
+
+@myapp.get('/blog/published')
+def published_blogs_list():
+    return {'data': 'All published blogs'}
+
+
+@myapp.get('/blog/published/{id}')
+def published_blog(id: int):
+    return {'data': id}
+
+
+@myapp.get('/blog/{id}/comments')
+def publishjed_blog_comments(id, limit: int = 10):
+    return {'data': {'1', '2'}}
+
+
+# Aquí van los métodos POST
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: bool
+    published_at: Optional[str]
+
+
+@myapp.post('/blog/')
+def create_blog(request: Blog):
+    return {'data': f'Blog has been created with {request.title} \
+            and {request.body}'}
